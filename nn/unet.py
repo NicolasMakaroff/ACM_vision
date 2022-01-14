aa0f30b50157d079529505e8fd32881264f15c06
NicolasMakaroff <nicolas.makaroff@ens-paris-saclay.fr>
2022-01-14 10:59:31 +0100

    add haussdorf

--- nn/unet.py
+++ nn/unet.py
@@ -5,11 +5,12 @@ import torch.nn as nn
 import numpy as np
 import wandb
 import pytorch_lightning as pl
-from utils.loss import ACMLoss, DiceLoss
+from utils.loss import ACMLoss, DiceLoss, HDDTBinaryLoss
 from utils.utils import  gray2rgb, wb_mask, image2np
 
 acm_loss = ACMLoss()
 dsc_loss = DiceLoss()
+hd_loss = HDDTBinaryLoss()
 
 class UNet(pl.LightningModule):
 
@@ -27,7 +28,7 @@ class UNet(pl.LightningModule):
         self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.bottleneck = UNet._block(features * 8, features * 16, name="bottleneck")
-        
+
 
         self.upconv4 = nn.ConvTranspose2d(
             features * 16, features * 8, kernel_size=2, stride=2
@@ -116,6 +117,7 @@ class UNet(pl.LightningModule):
         y_pred = self(x)
         #loss = acm_loss(y_pred, y_true)
         loss_info = dsc_loss(y_pred,y_true)
+        hdd = hd_loss(y_pred, y_true)
         logs={"train_loss": loss_info}
         batch_dictionary={
             #REQUIRED: It ie required for us to return "loss"
@@ -125,6 +127,7 @@ class UNet(pl.LightningModule):
             "log": logs,
         }
         self.log('loss', loss_info, prog_bar = False, on_step=False,on_epoch=True,logger=True)
+        self.log('haussdorf', hdd, prog_bar=False, on_step=False, on_epoch=True, logger=True)
         #self.log('dice', loss_info, prog_bar=False, on_step=False, on_epoch=True, logger=True)
 
         return loss_info
@@ -134,8 +137,10 @@ class UNet(pl.LightningModule):
         y_hat = self(x)
         #val_loss = acm_loss(y_hat, y)
         dsc = dsc_loss(y_hat,y)
+        hdd = hd_loss(y_hat, y)
         #self.log('val_loss', val_loss, prog_bar=False, on_step=False, on_epoch=True, logger=True)
         self.log('val_dsc',dsc, prog_bar=False, on_step=False,on_epoch=True, logger=True)
+        self.log('val_haussdorf',hdd, prog_bar=False, on_step=False,on_epoch=True, logger=True)
         return dsc
 
 class ImagePredictionLogger(pl.Callback):
--- utils/loss.py
+++ utils/loss.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import numpy as np
 import torch
+from scipy.ndimage import distance_transform_edt
 
 
 class DiceLoss(nn.Module):
@@ -50,3 +51,53 @@ class ACMLoss(nn.Module):
 
         return lenth + lambdaP * (mu * region_in + region_out)
 
+class HDDTBinaryLoss(nn.Module):
+    def __init__(self):
+        """
+        compute Hausdorff loss for binary segmentation
+        https://arxiv.org/pdf/1904.10030v1.pdf        
+        """
+        super(HDDTBinaryLoss, self).__init__()
+
+
+    def forward(self, net_output, target):
+        """
+        net_output: (batch_size, 2, x,y,z)
+        target: ground truth, shape: (batch_size, 1, x,y,z)
+        """
+        net_output = softmax_helper(net_output)
+        pc = net_output[:, 1, ...].type(torch.float32)
+        gt = target[:,0, ...].type(torch.float32)
+        with torch.no_grad():
+            pc_dist = compute_edts_forhdloss(pc.cpu().numpy()>0.5)
+            gt_dist = compute_edts_forhdloss(gt.cpu().numpy()>0.5)
+        # print('pc_dist.shape: ', pc_dist.shape)
+        
+        pred_error = (gt - pc)**2
+        dist = pc_dist**2 + gt_dist**2 # \alpha=2 in eq(8)
+
+        dist = torch.from_numpy(dist)
+        if dist.device != pred_error.device:
+            dist = dist.to(pred_error.device).type(torch.float32)
+
+        multipled = torch.einsum("bxyz,bxyz->bxyz", pred_error, dist)
+        hd_loss = multipled.mean()
+
+        return hd_loss
+
+
+def softmax_helper(x):
+    # copy from: https://github.com/MIC-DKFZ/nnUNet/blob/master/nnunet/utilities/nd_softmax.py
+    rpt = [1 for _ in range(len(x.size()))]
+    rpt[1] = x.size(1)
+    x_max = x.max(1, keepdim=True)[0].repeat(*rpt)
+    e_x = torch.exp(x - x_max)
+    return e_x / e_x.sum(1, keepdim=True).repeat(*rpt)
+
+def compute_edts_forhdloss(segmentation):
+    res = np.zeros(segmentation.shape)
+    for i in range(segmentation.shape[0]):
+        posmask = segmentation[i]
+        negmask = ~posmask
+        res[i] = distance_transform_edt(posmask) + distance_transform_edt(negmask)
+    return res
